645f4333233a76873a386c71d69a73d795c894a4
santhoshkganji <santhoshganji19@gmail.com>
2016-05-31 23:34:02 -0700

    First Git Commit

---
+++ short_reckonings/src/main/java/com/gap/shortreckonings/short_reckonings/App.java
@@ -0,0 +1,13 @@
+package com.gap.shortreckonings.short_reckonings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
